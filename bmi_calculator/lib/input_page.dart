2e5810e937288db792cce3951b59bc944d4560b5
someshkoli <kolisomesh27@gmail.com>
2020-04-16 05:13:45 +0530

    bmi app

---
+++ bmi_calculator/lib/input_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('BMI CALCULATOR')),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF1D1E33),
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableWidget(),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableWidget()
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableWidget()
+                  ),
+                  Expanded(
+                    child:ReusableWidget()
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableWidget extends StatelessWidget {
+  const ReusableWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          color: Color(0xFF1D1E33),
+          borderRadius: BorderRadius.circular(10)),
+    );
+  }
+}
