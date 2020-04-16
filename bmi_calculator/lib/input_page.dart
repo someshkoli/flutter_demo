60d17e7ac73792fdd126e92932b8ae3e3f0b8795
someshkoli <kolisomesh27@gmail.com>
2020-04-16 05:37:13 +0530

    bmi app styles added

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const double bottomComtainerHeight = 60;
+const activeCardColor = 0xFF1D1E33;
+const bottomContainerColor = 0xFFEB1555;
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -9,60 +13,76 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('BMI CALCULATOR')),
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                          color: Color(0xFF1D1E33),
-                          borderRadius: BorderRadius.circular(10)),
+      appBar: AppBar(
+        title: Center(child: Text('BMI CALCULATOR')),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: (){
+                      print("maincard was presed");
+                    },
+                    child: ReusableWidget(
+                      color_v: Color(0xFF1D1E33),
                     ),
                   ),
-                  Expanded(
-                    child: ReusableWidget(),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    child: ReusableWidget(
+                      color_v: Color(0xFF1D1E33),
+                    ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Expanded(
-              child: ReusableWidget()
+          ),
+          Expanded(
+            child: ReusableWidget(
+              color_v: Color(0xFF1D1E33),
             ),
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: ReusableWidget()
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableWidget(
+                    color_v: Color(0xFF1D1E33),
                   ),
-                  Expanded(
-                    child:ReusableWidget()
+                ),
+                Expanded(
+                  child: ReusableWidget(
+                    color_v: Color(0xFF1D1E33),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ));
+          ),
+          Container(
+            color: Color(0xFFEB1555),
+            height: bottomComtainerHeight,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+          )
+        ],
+      ),
+    );
   }
 }
 
 class ReusableWidget extends StatelessWidget {
-  const ReusableWidget({
-    Key key,
-  }) : super(key: key);
-
+  ReusableWidget({@required this.color_v});
+  final Color color_v;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15),
       decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10)),
+          color: color_v, borderRadius: BorderRadius.circular(10)),
     );
   }
 }
